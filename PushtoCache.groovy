184ae1df08190a0910c865df2c37317f8b51e4d6
Peter Antony <141098596+p-hatz@users.noreply.github.com>
2025-03-03 18:55:50 +1100

    Add files via upload

---
+++ PushtoCache.groovy
@@ -0,0 +1,24 @@
+import java.util.Properties;
+import java.io.InputStream;
+import com.boomi.execution.ExecutionUtil;
+import com.boomi.Cache;
+
+log = ExecutionUtil.getBaseLogger();
+
+def cache = new Cache();
+
+inData = ExecutionUtil.getDynamicProcessProperty("dppData");
+def _data = inData.split('\n')
+
+_data.each { _rec ->
+    def (name, val) = _rec.split('=')
+
+   Cache.set(_rec);
+}
+
+for( int i = 0; i < dataContext.getDataCount(); i++ ) {
+    InputStream is = dataContext.getStream(i);
+    Properties props = dataContext.getProperties(i);
+
+    dataContext.storeStream(is, props);
+}
